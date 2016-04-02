80d8d237f26a8000403183be3a9db5afe4edebe5
mirjamsk <mirjamskarica@gmail.com>
2016-04-02 22:04:42 +0200

    Add django REST framework

--- django_project/urls.py
+++ django_project/urls.py
@@ -2,12 +2,27 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 from sentiment.views import *
+
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
 #    url(r'^$', 'django_project.views.index', name='index'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', index, name='index'),
+    #url(r'^$', index, name='index'),
+    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
--- sentiment/views.py
+++ sentiment/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer
+
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world! Bok bok")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
