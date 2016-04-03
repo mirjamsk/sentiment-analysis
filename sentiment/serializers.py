368d68bd710405a38bc8e4ec4ea1ac9b34129c80
mirjamsk <mirjamskarica@gmail.com>
2016-04-03 16:53:08 +0200

    Remove HyperlinkedModelSerializer from all serializers to improve performance

--- sentiment/serializers.py
+++ sentiment/serializers.py
@@ -2,25 +2,25 @@ from rest_framework import serializers
 from .models import Post, Comment, PostSentiment, CommentSentiment
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ('id', 'content', 'link', 'likes', 'shares', 'comments')
 
 
-class PostSentimentSerializer(serializers.HyperlinkedModelSerializer):
+class PostSentimentSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostSentiment
         fields = ('id', 'idpost', 'sentiment', 'real_sentiment')
 
 
-class CommentSentimentSerializer(serializers.HyperlinkedModelSerializer):
+class CommentSentimentSerializer(serializers.ModelSerializer):
     class Meta:
         model = CommentSentiment
         fields = ('id', 'idpost', 'idcomment', 'sentiment', 'real_sentiment')
 
 
-class CommentSerializer(serializers.HyperlinkedModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ('id', 'idpost','content')
