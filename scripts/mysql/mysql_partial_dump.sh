35466f6690c384606e9ffb54210068a21992b741
mirjamsk <mirjamskarica@gmail.com>
2016-04-11 17:50:49 +0200

    Add scripts for updating comments real_sentiment from the command line, and a mysqldump command to dump data partially

---
+++ scripts/mysql/mysql_partial_dump.sh
@@ -0,0 +1,1 @@
+mysqldump --user=root --password=root sentiment_db im_commento_sentiment --replace --skip-add-drop-table -t --where="idcommento<'10'" > real_sentiment_dump.sql
---
+++ scripts/python/update_real_sentiment.py
@@ -0,0 +1,88 @@
+from db_utils import CommentDbConnection, CommentSentimentDbConnection
+from api_utils import TextProcessingApi, ViveknApi
+import argparse
+
+
+def get_input_label():
+    return raw_input("pos/neg/neu? ").strip()
+
+
+def update_real_sentiment_batch(select_where_clause="", db_name="sentiment_db"):
+    # Open database connections
+    # we need two: one for fetch records and another for update records)
+
+    SENTIMENT_LABELS = {
+        'pos': 'positive',
+        'neg': 'negative',
+        'neu': 'neutral'}
+    label_choices = set(SENTIMENT_LABELS.keys())
+
+    db = CommentDbConnection(db=db_name)
+    db.connect()
+
+    db_sentiment = CommentSentimentDbConnection(db=db_name)
+    db_sentiment.connect()
+
+    results = db.fetch_all(where=select_where_clause)
+
+    for row in results:
+        comment_id = row[0]
+        content = row[1]
+
+        print ("Comment_id: %s" % comment_id)
+        print ("Content: %s" % content)
+        print ("Determine the real_sentiment:")
+
+        real_sentiment = get_input_label()
+        while real_sentiment not in label_choices:
+            print("%s not in %s" %(real_sentiment, label_choices))
+            real_sentiment = get_input_label()
+
+        db_sentiment.update(
+            comment_id=comment_id,
+            sentiment=SENTIMENT_LABELS[real_sentiment],
+            sentiment_api_column='real_sentiment')
+
+        print ("Updated real_sentiment: %s" %SENTIMENT_LABELS[real_sentiment])
+
+        print (35 * "-")
+
+    db.close()
+    db_sentiment.close()
+
+
+def main():
+
+    parser = argparse.ArgumentParser(
+        description='Script for manually updating real_sentiment of comments')
+    parser.add_argument('-ideq',
+                        type=int,
+                        required=False,
+                        help='Update a specific comment by specifying it\'s id')
+    parser.add_argument('-idlt',
+                        type=int,
+                        required=False,
+                        help='Update all comments with id less than the specified id')
+    parser.add_argument('-idgt',
+                        type=int,
+                        required=False,
+                        help='Update all comments with id greater than the specified id')
+
+    args = parser.parse_args()
+
+    where_clause = ''
+    if args.ideq is not None:
+        where_clause = 'id=%d' % args.ideq
+    else:
+        if args.idlt is not None:
+            where_clause = 'id < %d' % args.idlt
+        if args.idgt is not None:
+            where_clause += ' AND ' if where_clause != '' else ''
+            where_clause += 'id > %d' % args.idgt
+
+    print (35 * "-")
+    update_real_sentiment_batch(select_where_clause=where_clause)
+
+
+if __name__ == '__main__':
+    main()
