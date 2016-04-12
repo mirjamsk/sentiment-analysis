aa3d077238db7413a85cb4930904db051b47ee7f
mirjamsk <mirjamskarica@gmail.com>
2016-04-12 21:29:16 +0200

    Finish translating comment content in english (export the data in sentiment_sqldump.sql)

--- scripts/python/utils/api_utils/google_translate_api.py
+++ scripts/python/utils/api_utils/google_translate_api.py
@@ -5,9 +5,10 @@ class GoogleTranslateAPI(BaseAPI):
     def __init__(self):
         self.language_column = 'language'
         self.english_translation_column = 'english_translation'
+        self.ApiKey = 'AIzaSyDXYzpYdHG8W2sGxd7e_E_zpci4S7pQNTg'
         BaseAPI.__init__(
             self,
-            url='https://www.googleapis.com/language/translate/v2?key=AIzaSyDXYzpYdHG8W2sGxd7e_E_zpci4S7pQNTg',
+            url='https://www.googleapis.com/language/translate/v2?key='+self.ApiKey,
             params={
                 'q': '',
                 'target': 'en',
