1c99177e2f7d5d2cf4b61caddda2e7eaa82f1815
mirjamsk <mirjamskarica@gmail.com>
2016-04-21 01:31:46 +0200

    Re-translate comments with correct charset, namely utf8mb4 (emojis supported)

--- scripts/python/utils/api_utils/google_translate_api.py
+++ scripts/python/utils/api_utils/google_translate_api.py
@@ -5,7 +5,8 @@ class GoogleTranslateAPI(BaseAPI):
     def __init__(self):
         self.language_column = 'language'
         self.english_translation_column = 'english_translation'
-        self.ApiKey = 'AIzaSyDXYzpYdHG8W2sGxd7e_E_zpci4S7pQNTg'
+        # self.ApiKey = 'AIzaSyDXYzpYdHG8W2sGxd7e_E_zpci4S7pQNTg' old api key
+        self.ApiKey = 'AIzaSyC-MWoE9Yn-9wx8zt3aoxR_ILIle1zhppM'
         BaseAPI.__init__(
             self,
             url='https://www.googleapis.com/language/translate/v2?key='+self.ApiKey,
