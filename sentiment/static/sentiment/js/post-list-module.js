3bcdd072ffaad85b4210bc344606a60105107466
mirjamsk <mirjamskarica@gmail.com>
2016-04-24 12:22:18 +0200

    Fix up and minify  materialize-pagination jQuery plugin

--- sentiment/static/sentiment/js/post-list-module.js
+++ sentiment/static/sentiment/js/post-list-module.js
@@ -119,6 +119,7 @@ $( function(){
                 align: 'center',
                 lastPage: data.lastPage ,
                 urlParameter:'post-page',
+				useUrlParameters:   true,
                 onClickCallback: paginationAPI.requestPage
             });
 
