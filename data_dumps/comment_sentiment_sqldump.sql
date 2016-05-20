b3f175e17726f2ba71bf3682e6d928b31411b865
mirjamsk <mirjamskarica@gmail.com>
2016-05-20 17:35:08 +0200

    Add commento_sentiment data dump with the new spam field

--- data_dumps/comment_sentiment_sqldump.sql
+++ data_dumps/comment_sentiment_sqldump.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:52a8cd987b6fc865bb21c4b15a94cff69f75cd6949347aa8f839450cbdfa88a9
-size 998644
+oid sha256:32b7c1c3b1b598cbcce884aafaf37448f9b3216e15fa5f0d597a2806c123daa0
+size 1260434
