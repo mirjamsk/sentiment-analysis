b2f3af88a59a7031ab566515eb8d805a6a9563ab
mirjamsk <mirjamskarica@gmail.com>
2016-05-24 13:35:46 +0200

    Dump post_sentiment_data with the new updated stats which ignore url spam comments

--- data_dumps/post_sentiment_sqldump.sql
+++ data_dumps/post_sentiment_sqldump.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1805b724db0d262887cdf8f92cb495e83ef3bb67e075077a0d4ea494a877f1a2
-size 106804183
+oid sha256:e6a9954496128fdf6700f18f5d11ac6d5dd4cc564ab8645afe31b1c80eb2d157
+size 106800976
